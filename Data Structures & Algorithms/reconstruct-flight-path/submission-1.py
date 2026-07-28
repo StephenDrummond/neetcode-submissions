b0be8782fb2bc1699b70d85f100252c214c9940a
StephenDrummond <124301182+StephenDrummond@users.noreply.github.com>
2026-07-28 12:00:57 -0400

    Add: reconstruct-flight-path - submission-1

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-1.py
@@ -0,0 +1,23 @@
+import bisect
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        adj = defaultdict(list)
+        for i in range(len(tickets)):
+            depart, dest = tickets[i]
+            bisect.insort(adj[depart], (dest, i))
+        ans = ["JFK"]
+        n = len(tickets) + 1
+
+        def dfs(cur, seen):
+            if len(cur) == n:
+                return cur
+            for nei, idx in adj[cur[-1]]:
+                if idx not in seen:
+                    seen.add(idx)
+                    cur.append(nei)
+                    if dfs(cur, seen):
+                        return cur
+                    cur.pop()
+                    seen.remove(idx)
+            
+        return dfs(["JFK"], set())
