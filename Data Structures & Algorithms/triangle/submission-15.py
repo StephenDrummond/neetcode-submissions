820f6527042ff7edd43caee626733c1d88aecac0
StephenDrummond <124301182+StephenDrummond@users.noreply.github.com>
2026-08-03 12:04:32 -0400

    Add: triangle - submission-15

---
+++ Data Structures & Algorithms/triangle/submission-15.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)
+        dp = triangle[-1][:]
+        
+
+        for row in range(n - 2, -1, -1):
+            for col in range(len(triangle[row])):
+                dp[col] = min(dp[col] + triangle[row][col], dp[col+1] + triangle[row][col])
+            
+
+        return dp[0]
