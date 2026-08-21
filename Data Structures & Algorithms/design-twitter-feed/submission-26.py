56d818fac9ec02ec6e321ba4aedd13deac558ed3
StephenDrummond <124301182+StephenDrummond@users.noreply.github.com>
2026-08-21 11:10:35 -0400

    Add: design-twitter-feed - submission-26

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-26.py
@@ -0,0 +1,28 @@
+class Twitter:
+
+    def __init__(self):
+        self.following = defaultdict(set)
+        self.tweets = defaultdict(list)
+        self.time = 0
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        heapq.heappush(self.tweets[userId], (self.time, tweetId))
+        self.time -= 1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        timeline = []
+        following = self.following[userId].copy()
+        following.add(userId)
+        for f in following:
+            for tweet in self.tweets[f]:
+                heapq.heappush(timeline, tweet)
+        return [heapq.heappop(timeline)[1] for _ in range(min(10, len(timeline)))]
+        
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followeeId == followerId:
+            return
+        self.following[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        self.following[followerId].discard(followeeId)
