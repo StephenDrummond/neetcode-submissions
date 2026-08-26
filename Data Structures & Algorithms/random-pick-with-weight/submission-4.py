5f4346d352728cd02e1860c99535c5b616077582
StephenDrummond <124301182+StephenDrummond@users.noreply.github.com>
2026-08-26 12:50:42 -0400

    Add: random-pick-with-weight - submission-4

---
+++ Data Structures & Algorithms/random-pick-with-weight/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+
+    def __init__(self, w: List[int]):
+        self.w = []
+        for i in range(len(w)):
+            for j in range(w[i]):
+                self.w.append(i)
+        print(self.w)
+
+    def pickIndex(self) -> int:
+        return random.choice(self.w)
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
